2dbb1013d4d94a0d1816b1d42f40b2247227099d
ccatura <charlie@charliecatura.com>
2023-07-21 11:33:19 -0400

    creating help message page

--- help.php
+++ help.php
@@ -6,7 +6,7 @@
     $subject = 'Help request from MMIN8';
 
     if (!empty($_POST)) {
-        $message     = "Message from: {$from} ($name)<br>";
+        $message     = "Message from: {$from} ({$name})<br>";
         $message    .= $_POST['message'];
         email($from, $name, $to, $subject, $message);
         // header('Location: ' . $_SERVER['HTTP_REFERER']);
@@ -16,6 +16,7 @@
         echo   "<form class='message-single' action='#' method='post'>
                 Send message to<strong>Help</strong><br>
                 <input type='hidden' name='to' value='{$to}'>
+                From<strong>{$from} ({$name})</strong><br>
                 Subject<strong>Need Help at MeetMeInThe80s.com</strong><br>
                 Describe your issue
                 <textarea name='message' required></textarea><br>
