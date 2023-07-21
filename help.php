81279907235731210140f78bd8a80356d74e4b42
ccatura <charlie@charliecatura.com>
2023-07-21 11:38:43 -0400

    creating help message page

--- help.php
+++ help.php
@@ -9,6 +9,7 @@
         $message     = "Message from: {$from} ({$name})<br>";
         $message    .= $_POST['message'];
         email($from, $name, $to, $subject, $message);
+        message($conn, $from, $to, $subject, $message);
         $_SESSION['message'] = "Your help request was sent.";
         header('Location: ' . $_SERVER['HTTP_REFERER']);
 
