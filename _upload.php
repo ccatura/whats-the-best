b084417f8858f01eb68d717363680d164b4c8f9b
ccatura <charlie@charliecatura.com>
2023-07-26 12:04:00 -0400

    trying to image-resize

--- _upload.php
+++ _upload.php
@@ -45,15 +45,18 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
 
-  $image_name  = $target_file;
-  $image       = imagecreatefromjpeg ($image_name);
-  $imgResized  = imagescale($image , 100, -1);
-  $target_file = imagejpeg($imgResized, $target_file);
+
+
+
 
 
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 
+    $image_name  = $target_file;
+    $image       = imagecreatefromjpeg ($image_name);
+    $imgResized  = imagescale($image , 100, -1);
+    $target_file = imagejpeg($imgResized, $target_file);
 
 
     move_uploaded_file($target_file, '_'.$target_file);
