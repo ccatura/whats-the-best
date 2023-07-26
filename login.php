3004d47ec84b42cbca13b7dcbf6316eb0b997905
ccatura <charlie@charliecatura.com>
2023-07-26 19:08:36 -0400

    working on image upload and account update image upload

--- login.php
+++ login.php
@@ -101,7 +101,7 @@ if (!empty($_POST)) {
                 }
 
                 // Check file size
-                if ($_FILES["fileToUpload"]["size"] > 500000) {
+                if ($_FILES["fileToUpload"]["size"] > 5000000) {
                     echo "Sorry, your file is too large.";
                     $uploadOk = 0;
                 }
