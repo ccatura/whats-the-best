af331fcc440840fbecac231b71cff4d415109d4b
ccatura <charlie@charliecatura.com>
2023-07-27 11:04:38 -0400

    working out multiple image uploads

--- login.php
+++ login.php
@@ -75,8 +75,7 @@ if (!empty($_POST)) {
             email('ccatura', 'Charles Catura', 'ccatura@gmail.com', $subject, $message);
             $_SESSION['how-to'] = true;
 
-            echo 'file: ' . $_FILES['file-to-upload'];
-            if ($_FILES['file-to-upload']) {
+            if (!empty($_FILES['file-to-upload']['name'])) {
                 prepare_images_uploaded($_FILES, $user_name);
             } else {
                 // Assign generic profile pics
